7264698eb459512b4fcadaa1aa1fc1a6a31c2eda
opti93 <vitaliygovarkov@gmail.com>
2022-11-12 20:22:27 +0200

    lesson12_3

--- Lesson-12/main.tf
+++ Lesson-12/main.tf
@@ -46,10 +46,10 @@ resource "aws_security_group" "my_server" {
   dynamic "ingress" {
     for_each = ["80", "443"]
     content {
-      from_port  = ingress.value
-      to_port    = ingress.value
-      protocol   = "tcp"
-      cidr_block = ["0.0.0.0/0"]
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
   egress {
